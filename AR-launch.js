e92c19faad4f31f3c57e35d9a94a37ff2ddcf1a1
Nigel A <nigel@bgcreatives.com>
2015-05-28 14:27:00 -0400

    added AR commands to gesture control.

--- AR-launch.js
+++ AR-launch.js
@@ -1,16 +1,57 @@
 var arDrone = require('ar-drone');
 var client = arDrone.createClient();
+client.config({
+  'control:altitude_max': 1000
+});
 
 client.takeoff();
 
-client
-  .after(5000, function() {
-    this.clockwise(0.5);
-  })
-  .after(3000, function() {
-    this.animate('flipLeft', 15);
-  })
-  .after(1000, function() {
+client.after(2000, function() {
+    this.up(.2);
+  });
+client.after(2500, function(){
+  this.stop();
+})
+client.after(3000, function(){
+  this.down(.2);
+})
+client.after(4000, function(){
+  /*this.left(.2);
+  this.stop();
+  this.animate('flipLeft', 1000);
+  this.stop();*/
+  this.land();
+});
+/*client.after(5500, function(){
+  this.stop();
+});
+*/
+client.after(6000, function(){
+  this.takeoff();
+})
+
+  /*client.after(4000, function(){
+    this.animate('flipLeft', 1000);
+    this.stop();
+    this.land()
+  });*/
+
+  /*client.after(6500, function(){
+    this.animate('flipRight', 1000);
+    this.stop();
+  });
+
+  client.after(8500, function(){
+    this.animate('flipAhead', 1000);
+    this.stop();
+  });
+
+  client.after(15000, function(){
+    this.animate('flipBehind', 1000);
     this.stop();
     this.land();
   });
+
+client.after(5000, function(){
+  this.land();
+});*/
--- routes/socketRoutes.js
+++ routes/socketRoutes.js
@@ -1,25 +1,40 @@
 
+var client = arDrone.createClient();
 
+client.config({
+  'control:altitude_max': 1000
+});
 
+client.takeoff();
 
 module.exports = function( socket ){
 
     socket.on( "Right" , function( data ){
-
+        client.right(.1);
+        client.after(1000, function(){
+          this.stop();
+        });
         console.log( "Right" );
 
     } )
     socket.on( "Left" , function( data ){
-
+      client.left(.1);
+      client.after(1000, function(){
+        this.stop();
+      });
         console.log( "Left" );
 
     } )
-    socket.on( "Land" , function( data ){
-
-        console.log( "Land" );
+    socket.on( "Take Off" , function( data ){
+        client.takeoff();
+        console.log( "taking off now" );
 
     } )
     socket.on( "Down" , function( data ){
+      client.land();
+      client.after(1000, function(){
+        this.stop();
+      });
 
         console.log( "Down" );
 
