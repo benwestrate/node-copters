e1e3d046a4108a0403153a31e09b234586e32290
Ben Westrate <bwestrate@citytechinc.com>
2015-05-28 14:32:00 -0400

    added in ar drone dep

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser      = require('body-parser');
 
 var routes          = require('./routes/index');
 var users           = require('./routes/users');
-var arDrone         = require('ar-drone');
 
 var app = express();
 
